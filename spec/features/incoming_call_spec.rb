b272d0d073b813909654d093dae9b124594f3862
Mike Menne <mike@humanagency.org>
2019-04-21 20:15:01 -0500

    Fixing a few broken tests :-(

--- spec/features/incoming_call_spec.rb
+++ spec/features/incoming_call_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 
 RSpec.describe 'Twilio Incoming Call API', type: :feature do
   it 'connects to another number' do
-    FactoryBot.create(:transplant_center, :slu)
     from_number = "+#{Faker::Number.number(11)}"
     to_number = "+#{Faker::Number.number(11)}"
+    FactoryBot.create(:transplant_center, :slu, phone_number: to_number)
     @call = ttt_call('api/v1/incoming_calls', from_number, to_number, caller_state: 'MO')
 
     expect(@call).to have_say('Thank you for considering organ donation.  Now connecting you to St. Louis University Hospital')
