540fe9a4b26314afc0fe6171a9c21c75613527f1
Mike Menne <mike@humanagency.org>
2019-04-21 20:46:32 -0500

    Moving data to seeds file

--- db/migrate/20190418063201_create_transplant_centers.rb
+++ db/migrate/20190418063201_create_transplant_centers.rb
@@ -9,26 +9,5 @@ class CreateTransplantCenters < ActiveRecord::Migration[5.2]
 
       t.timestamps
     end
-    TransplantCenter.create(
-      name: 'Saint Louis University Hospital',
-      phone_number: '+13145778867',
-      area_code: '314',
-      city: 'Saint Louis',
-      state: 'MO'
-    )
-    TransplantCenter.create(
-      name: 'Barnes Jewish Hospital',
-      phone_number: '+118668894376',
-      area_code: '314',
-      city: 'Saint Louis',
-      state: 'MO'
-    )
-    TransplantCenter.create(
-      name: 'Cedars Sinai Hospical',
-      phone_number: '+13104232641',
-      area_code: '310',
-      city: 'Los Angeles',
-      state: 'CA'
-    )
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  TransplantCenter.create(
+    name: 'Saint Louis University Hospital',
+    phone_number: '+13145778867',
+    area_code: '314',
+    city: 'Saint Louis',
+    state: 'MO'
+  )
+  TransplantCenter.create(
+    name: 'Barnes Jewish Hospital',
+    phone_number: '+118668894376',
+    area_code: '314',
+    city: 'Saint Louis',
+    state: 'MO'
+  )
+  TransplantCenter.create(
+    name: 'Cedars Sinai Hospical',
+    phone_number: '+13104232641',
+    area_code: '310',
+    city: 'Los Angeles',
+    state: 'CA'
+  )
